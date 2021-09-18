ef29fe9ce8efd773976c97aede34bc32e4d5cada
Ri-Hong <riri.hong@gmail.com>
2021-09-18 17:36:42 -0400

    Flex

--- app/screens/ASLConverterScreen.js
+++ app/screens/ASLConverterScreen.js
@@ -1,8 +1,8 @@
 // screen where ASL gets converted into English
 import React from 'react'
-import { Text} from 'react-native';
 
 import {
+  SafeAreaView,
   View,
   Image,
   Button
@@ -15,7 +15,7 @@ const ASLConverterScreen = () => {
   return (
 
     // This View is the background
-    <View
+    <SafeAreaView
       style={{
         backgroundColor: "#e4eed6ff",
         flex: 1
@@ -54,31 +54,25 @@ const ASLConverterScreen = () => {
           style={{
             flexDirection: 'row',
             alignItems: "center",
+            border: "3px solid red",
           }}
         >
           <Button
             title="Clear"
             onPress={() => console.log("Clear")}
+            style={{
+              paddingRight: 10
+            }}
           ></Button>
 
-          {/* Some padding */}
-          <View
-            style={{
-              paddingLeft: 10
-            }}
-          ></View>
 
           <Button
             title="Read"
             onPress={() => console.log("Read")}
-          ></Button>
-
-          {/* Some padding */}
-          <View
             style={{
-              paddingLeft: 20
+              paddingRight: 20
             }}
-          ></View>
+          ></Button>
 
         </View>
 
@@ -87,13 +81,14 @@ const ASLConverterScreen = () => {
       {/* This View is the white background where text will be displayed */}
       <View
         style={{
-          width: "90%",
-          height: "80%",
-          backgroundColor: "white"
+          flex: 1,
+          backgroundColor: "white",
+          marginLeft: "auto",
+          marginRight: "auto",
         }}
       ></View>
 
-    </View>
+    </SafeAreaView>
   )
 }
 
