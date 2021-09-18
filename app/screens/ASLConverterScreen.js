c9ab36cee264c4a7dbe7ddd0fddb42b79f966132
Winswins22 <73000223+Winswins22@users.noreply.github.com>
2021-09-18 15:51:03 -0400

    init

---
+++ app/screens/ASLConverterScreen.js
@@ -0,0 +1,12 @@
+// screen where ASL gets converted into English
+import React from 'react'
+
+const ASLConverterScreen = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default ASLConverterScreen
