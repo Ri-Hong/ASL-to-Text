c5ab7f33949e61ac38a58bfe8e123468a5dc11ba
Winswins22 <73000223+Winswins22@users.noreply.github.com>
2021-09-19 03:08:17 -0400

    add json support

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const data = require("./data.json");
+
+let port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+  res.send("Hello, World!");
+})
+
+app.get('/test', (req, res) => {
+  res.send(data);
+})
+
+app.post('/sms', (req, res) => {
+  
+  console.log(res.body);
+  
+  res.send("Successful retrival of body ", res.body);
+})
+
+app.listen(port, () => {
+  console.log("App is listening on port ", port);
+})
