012b3ee6df2220cdb9a5572c8516f2d317b3673b
Winswins22 <73000223+Winswins22@users.noreply.github.com>
2021-09-19 01:37:09 -0400

    fix GET requests

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const data = require("./data.json");
 
 let port = process.env.PORT || 3000;
 
-app.get('./', (req, res) => {
+app.get('/', (req, res) => {
   res.send("Hello, World!");
 })
 
-app.get('./test', (req, res) => {
+app.get('/test', (req, res) => {
   res.send(data);
 })
 
