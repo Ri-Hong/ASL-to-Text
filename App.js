455c6075c688fa4a0e8e0c20be0c4f7dedd199bf
Simon <xxsimonxx119@gmail.com>
2021-09-19 09:44:34 -0400

    fixed camera

--- App.js
+++ App.js
@@ -10,12 +10,28 @@ export default function App() {
 	If useRef is use, Letter doesn't get update.
 	*/
 	const ASLletter = useRef("");
+	const webcam = useRef("");
+
+	async function HandleCloseCam() {
+		await webcam.current.pause();
+		console.log("WebCamera pause");
+	}
+
+	async function HandleOpenCam() {
+		await webcam.current.play();
+		console.log("WebCamera open");
+	}
+
 	return (
 		<>
 			<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js"></script>
 			<script src="https://cdn.jsdelivr.net/npm/@teachablemachine/image@0.8.3/dist/teachablemachine-image.min.js"></script>
-			<ASLConverterScreen ASLletter={ASLletter} />
-			<ASLtoText ASLletter={ASLletter} />
+			<ASLConverterScreen
+				ASLletter={ASLletter}
+				HandleOpenCam={HandleOpenCam}
+				HandleCloseCam={HandleCloseCam}
+			/>
+			<ASLtoText ASLletter={ASLletter} WEBCAM={webcam} />
 			{console.log(ASLletter.current)}
 		</>
 	);
